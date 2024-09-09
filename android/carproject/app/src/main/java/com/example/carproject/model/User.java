a381924029964ef42d98af02be8000851d917a4f
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-09-09 16:30:30 -0300

    NEW: user class in package model

---
+++ android/carproject/app/src/main/java/com/example/carproject/model/User.java
@@ -0,0 +1,54 @@
+package com.example.carproject.model;
+
+import java.util.Objects;
+
+public class User {
+
+    private String email; // email do usuário - é chave primária no banco
+    private String password; // password de acesso
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
