3c0bd16a54a4285813e3ae15903387e2c2fd19da
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-09-11 17:12:26 -0300

    ENH: Interfaces CarListView e LoginInterface movidas para package view

--- android/carproject/app/src/main/java/com/example/carproject/view/CarListView.java
+++ android/carproject/app/src/main/java/com/example/carproject/view/CarListView.java
@@ -1,4 +1,4 @@
-package com.example.carproject;
+package com.example.carproject.view;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.carproject.R;
 
 public class CarListView extends AppCompatActivity {
 
